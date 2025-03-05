db7711d0083a3a39e4527daff33c5648a7aee07c
Eric <eric.1334@icloud.com>
2025-03-04 17:05:16 -0800

    Bump lean and mathlib

--- AutLinOrd.lean
+++ AutLinOrd.lean
@@ -1,3 +1,3 @@
--- This module serves as the root of the `AutLinOrd` library.
--- Import modules here that should be built as part of the library.
-import «AutLinOrd».Basic
+import AutLinOrd.Basic
+import AutLinOrd.MulAction
+import AutLinOrd.OrdClosure
